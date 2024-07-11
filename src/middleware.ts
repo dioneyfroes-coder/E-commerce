a5b1564d6a77c6140b0d2342495f47019f955560
dioneyfroes-coder <dioney.froes@hotmail.com>
2024-07-11 16:58:41 -0300

    fix middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,10 +1,21 @@
-// src/middleware.ts
-
+import { NextResponse } from "next/server";
 import { clerkMiddleware } from "@clerk/nextjs/server";
 
-export default clerkMiddleware({
-  publicRoutes: ["/", "/product(.*)", "/cart", "/error(.*)", "/NotFound(.*)", "/seach(.*)"],
+const publicRoutes = ["/", "/product(.*)", "/cart", "/error(.*)", "/NotFound(.*)", "/search(.*)"];
+
+const middleware = clerkMiddleware((req: any, evt: any) => {
+  const url = req.nextUrl;
+
+  // Verifique se a rota é pública
+  if (publicRoutes.some((route) => new RegExp(route).test(url.pathname))) {
+    return NextResponse.next();
+  }
+
+  // Verifique a autenticação para as rotas privadas
+  return NextResponse.rewrite(new URL('/login', req.url));
 });
+
+export default middleware;
 
 export const config = {
   matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
