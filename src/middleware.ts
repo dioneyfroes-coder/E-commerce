f6934d15c7727f599b7edc5dee67d67b536f7a00
dioneyfroes-coder <dioney.froes@hotmail.com>
2024-07-11 20:34:44 -0300

    middleware fix 1

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,22 +1,9 @@
-import { NextResponse } from "next/server";
-import { clerkMiddleware } from "@clerk/nextjs/server";
+import { clerkMiddleware } from '@clerk/nextjs/server';
 
-const publicRoutes = ["/", "/product(.*)", "/cart", "/error(.*)", "/NotFound(.*)", "/search(.*)"];
-
-const middleware = clerkMiddleware((req: any, evt: any) => {
-  const url = req.nextUrl;
-
-  // Verifique se a rota é pública
-  if (publicRoutes.some((route) => new RegExp(route).test(url.pathname))) {
-    return NextResponse.next();
-  }
-
-  // Verifique a autenticação para as rotas privadas
-  return NextResponse.rewrite(new URL('/login', req.url));
-});
-
-export default middleware;
+export default clerkMiddleware()
 
 export const config = {
-  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
+  // The following matcher runs middleware on all routes
+  // except static assets.
+  matcher: [ '/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
 };
